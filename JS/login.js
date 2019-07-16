2bf7eef5f9a870f2637437a8b02c5ce1327938bc
mianma <mianmabb@163.com>
2019-07-16 08:57:06 +0800

    完善login.html

--- JS/login.js
+++ JS/login.js
@@ -19,8 +19,21 @@ $(function(){
       $(this).html("更多第三方登录方式 ∨");
       $(this).attr("opened", "0");
     }
-
-
   });
+  $("#lp_gozc").click(function(){
+    if($(this).attr("opened")==0){
+      $("#lp_zc").slideDown();
+      $(this).html("已有账号？马上登陆 ∧");
+      $(this).attr("opened","1");
+      $("#lp_login").slideUp();
+      $(".lp_title").html('<span class="lp_t_left"></span>登录美食杰<span class= "lp_t_right"></span>');
+    }else{
+      $("#lp_zc").slideUp();
+      $(this).html("还没有账号？免费注册 ∨");
+      $(this).attr("opened", "0");
+      $("#lp_login").slideDown();
+      $(".lp_title").html('<span class="lp_t_left"></span>注册美食杰<span class= "lp_t_right"></span>');
+    }
+  })
 
 })
