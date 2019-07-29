703bcd117fc478f682cd1b9e465b5b4b92316dab
mianma <mianmabb@163.com>
2019-07-29 21:10:53 +0800

    解决了ajax错误

--- JS/add.js
+++ JS/add.js
@@ -1,15 +1,19 @@
 $(function(){
   $.ajax({
-    url:"../HTML/header.html",
+    url:"header.html",
     type:"get",
+    // dataType:"text",
     success:function(result){
       $(result).replaceAll("#header");
       $(`<link rel="stylesheet" href="../CSS/header.css">`).appendTo("head");
-      $(`<script src="JS/header.js"></script>`).appendTo("head");
+      $(`<script src="../JS/header.js"></script>`).appendTo("head");
     }
   })
+  // $('#header').load('header.html',function() {
+  //   console.log(1);
+  // })
   $.ajax({
-    url:"../HTML/footer.html",
+    url:"footer.html",
     type:"get",
     success:function(result){
       $(result).replaceAll("#footer");
